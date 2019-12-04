f7e788972ac190e166383130ec5ecf191b88eab4
Alethia Quintero <alethiaq22@gmail.com>
2019-12-04 01:11:04 -0500

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -50,6 +50,18 @@ class Timer extends Component {
   handleClose = () => {
     this.props.removeTimer(this.props.id);
   };
+
+  // change the font color randomly. 
+  componentDidUpdate() {
+    this.timer.current.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  };
+// prevents unnecessary updates being caused by App's state changes
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  }
 }
 
 export default Timer;
